3bf16bed7e1d008f970c5d12048fc1d1717ca074
salvatore sorrentino <salvatore.sorrentino@live.com>
2023-09-28 11:45:08 +0200

    IAsyncEnumerable

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// diverso da foreach(var line in await ReadLinesAsync("got.json"))
+
+await foreach (var line in ReadLinesAsync("got.json"))
+{
+    Console.WriteLine(line);
+}
+
+async IAsyncEnumerable<string> ReadLinesAsync(string path)
+{
+    using var stream = new StreamReader(File.OpenRead(path));
+    string? line;
+    while ((line = await stream.ReadLineAsync()) != null)
+    {
+        await Task.Delay(500);
+        yield return line;
+    }
+}
