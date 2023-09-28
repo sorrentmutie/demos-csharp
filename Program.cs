a772537208012703ab661d9e09ff8b5b254906b4
salvatore sorrentino <salvatore.sorrentino@live.com>
2023-09-28 14:39:45 +0200

    record

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var p = new Person("Mario", "Rossi");
+var q = new Person("Mario", "Rossi");
+
+var r = p with { FirstName = "Luigi" };
+
+System.Console.WriteLine(p == q);
+
+record Person(string FirstName, string LastName) {}
+
+// EF Core relies on reference types to track changes
